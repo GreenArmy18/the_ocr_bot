8fa120f8c8fc349367521a6060b4d5d9a60fd786
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-10 23:34:28 +0200

    upsi

--- ocrbot/commands/help.py
+++ ocrbot/commands/help.py
@@ -10,10 +10,3 @@ def help(update:Update,context:CallbackContext):
         \n/start - To start the bot\
         \n/help - To show this message",quote=True
     )
-
-@send_typing_action
-def helping(update:Update,context:CallbackContext):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text(
-            'כדאי שתתקשרי לאופק, הוא כבר ידע לעזור לך.\n או שפשוט תשלחי לו הודעה: https://t.me/The_Horizon18',quote=True
-    )
--- ocrbot/helpers/mock_database.py
+++ ocrbot/helpers/mock_database.py
@@ -11,17 +11,17 @@ my_database = {}
 }
 '''
 
-def insert_file_path(chat_id:int, file_path:str) -> None:
+def insert_file_path(chat_id:int, message_id:int, file_path:str) -> None:
     try:
-        my_database[chat_id] = file_path
+        my_database[chat_id][message_id] = file_path
     except KeyError:
         my_database[chat_id] = {}
-        my_database[chat_id] = file_path
+        my_database[chat_id][message_id] = file_path
 
 def get_file_path(chat_id:int, message_id:int):
     try:
-        file_path = my_database[chat_id]
-        del my_database[chat_id]
+        file_path = my_database[chat_id][message_id]
+        del my_database[chat_id][message_id]
         return file_path
     except KeyError:
         return None
