e5bf32f1395e4fdd664da5e5fc6a5f521cb7fe63
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-10 23:13:30 +0200

    Update callbackquery_handler.py

--- ocrbot/handlers/callbackquery_handler.py
+++ ocrbot/handlers/callbackquery_handler.py
@@ -17,6 +17,7 @@ def next_weekday(d, weekday):
 
 d = date.today() + timedelta(days=1)
 next_thursday = next_weekday(d, 3) # 0 = Monday, 1=Tuesday, 2=Wednesday...
+tommorw_date=next_thursday.strftime("%d/%m/%y")
 
 @send_typing_action
 def button_click(update:Update,context:CallbackContext):
@@ -33,11 +34,7 @@ def button_click(update:Update,context:CallbackContext):
         
         if data['IsErroredOnProcessing']==False:
             message=data['ParsedResults'][0]['ParsedText']
-            print(calculate(message.splitlines()),"final result")
             total_hours_end, total_minutes_end, hours,minutes=calculate(message.splitlines())
-            #query.edit_message_text(f"{message}")
-            #query.edit_message_text(f"{message}")
-            tommorw_date=next_thursday.strftime("%d/%m/%y")
             query.edit_message_text(text='שבוע טוב, אימא\n''השבוע עבדת '+total_hours_end+' שעות ו־'+total_minutes_end+' דקות.\n''ביום חמישי הקרוב – '+tommorw_date+', תצטרכי לעבוד ' +hours+ ' שעות ו־' +minutes+ ' דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)')
 
         else:
@@ -60,7 +57,7 @@ def calculate(data_list):
 
         for i in range (2):
             hours.append((str1[pos_flags[i]-2:pos_flags[i]+3]).split(" "))
-    print(hours,"hours")
+
     from datetime import datetime
 
     for x in range(1,5):
@@ -75,7 +72,6 @@ def calculate(data_list):
 
     test=total_time
     test = '%02d:%02d:%02d.%06d' % (test.days*24 + test.seconds // 3600, (test.seconds % 3600) // 60, test.seconds % 60, test.microseconds)
-    print(test,"test")
 
     total_hours_end=(str(test)[:-3].split(":"))[0]
     total_minutes_end=(str(test)[:-3].split(":"))[1]
@@ -88,16 +84,4 @@ def calculate(data_list):
     if total_minutes[:-1]=='0':
         total_minutes=total_minutes[1:]
 
-    #total_hours_end, total_minutes_end, hours,minutes = somefunction(f.read())
-    #total_hours_end = '%02d:%02d:%02d.%06d' % (total_hours_end.days*24 + total_hours_end.seconds // 3600, (total_hours_end.seconds % 3600) // 60, total_hours_end.seconds % 60, total_hours_end.microseconds)
-
-    print(total_hours_end, total_minutes_end)
-    #tommorw_date=(date.today() + timedelta(days=1)).strftime("%d/%m/%y")
-    tommorw_date=next_thursday.strftime("%d/%m/%y")
-    #context.bot.send_message(chat_id=update.message.chat_id, text='שבוע טוב, אימא\n''השבוע עבדת '+total_hours_end+' שעות ו־'+total_minutes_end+' דקות.\n''ביום חמישי הקרוב – '+tommorw_date+', תצטרכי לעבוד ' +hours+ ' שעות ו־' +minutes+ ' דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)')
-    print('שבוע טוב, אימא\n''השבוע עבדת ',total_hours_end,' שעות ו־',total_minutes_end,' דקות.\n''ביום חמישי הקרוב – ',tommorw_date,', תצטרכי לעבוד ' ,total_hours, ' שעות ו',total_minutes,'־דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)')
-    text='שבוע טוב, אימא\n''השבוע עבדת ',total_hours_end,' שעות ו־',total_minutes_end,' דקות.\n''ביום חמישי הקרוב – ',tommorw_date,', תצטרכי לעבוד ' ,total_hours, ' שעות ו',total_minutes,'־דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)'
-    #'שבוע טוב, אימא\n''השבוע עבדת ',total_hours_end+' שעות ו־'+total_minutes_end+' דקות.\n''ביום חמישי הקרוב – '+tommorw_date+', תצטרכי לעבוד ' +total_hours+ ' שעות ו־' +total_minutes+ ' דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)'
-    print(total_hours_end, total_minutes_end, total_hours, total_minutes,"my numbers")
     return total_hours_end, total_minutes_end, total_hours, total_minutes
-    #return text
