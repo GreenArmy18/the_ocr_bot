6ac167ab9ee19715cb266e38e863d43e042191d2
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-14 14:22:19 +0200

    Update extract_image.py

--- ocrbot/handlers/extract_image.py
+++ ocrbot/handlers/extract_image.py
@@ -115,7 +115,7 @@ def extract_image(update:Update,context:CallbackContext):
             img.save(image_file, format='PNG', quality=95)
             image_file.seek(0)  # important, set pointer to beginning after writing image
             print("ready to send")
-            update.message.reply_document(document=image_file)
+            m.edit_media(document=image_file, quote=True)
 
             #crop_img = img[y:y+h, x:x+w]
 
