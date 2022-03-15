f6d498522efe52d600c01ad5065e3ff31f01137c
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-15 15:08:32 +0200

    Update extract_image.py

--- ocrbot/handlers/extract_image.py
+++ ocrbot/handlers/extract_image.py
@@ -69,8 +69,8 @@ def extract_image(update:Update,context:CallbackContext):
             size=len(data['ParsedResults'][0]['TextOverlay']['Lines'])
             for x in range(size):
             #if data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['WordText']==today:
-            #if data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['WordText']==30:
-                l,t= data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['Left'], data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['Top']
+                if data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['WordText']=='02':
+                    l,t= data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['Left'], data['ParsedResults'][0]['TextOverlay']['Lines'][x]['Words'][0]['Top']
             
             #l=391
             l+=45
