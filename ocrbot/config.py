683fc9061e034e926fee95129bbdeda008fb5d21
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-15 21:11:47 +0200

    Update config.py

--- ocrbot/config.py
+++ ocrbot/config.py
@@ -2,6 +2,7 @@ import os
 import dotenv
 
 #dotenv.load_dotenv()
-
-API_KEY = "K82520867588957"
+#8adf27cb6488957
+#K82520867588957
+API_KEY = "8adf27cb6488957"
 BOT_TOKEN = "5198784690:AAEcpZIGDIY6HhXoL8U_fK6qYVCzbmGFy4A"
