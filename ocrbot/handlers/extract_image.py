01f1165bfcf42870877f2874c1ccacfd3b70087e
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-16 18:59:20 +0200

    Update extract_image.py

--- ocrbot/handlers/extract_image.py
+++ ocrbot/handlers/extract_image.py
@@ -16,71 +16,8 @@ def next_weekday(d, weekday):
         days_ahead += 7
     return d + timedelta(days_ahead)
 
-@send_typing_action
-def extract_image(update:Update,context:CallbackContext):
-    '''
-    This function is called when the user sends a photo.
-    '''
-    chat_id=update.effective_chat.id
-    file_id = update.message.photo[-1].file_id
-    print(file_id,'file_id')
-    newFile=context.bot.get_file(file_id)
-    print(newFile,'newFile')
-    file_path= newFile.file_path
-    print(file_path,'file_path')
-    m = update.message.reply_text('מנתח תמונה, רק רגע...',quote=True)
-    
-    print(update.message.photo[-1].height,update.message.photo[-1].width)
-
-    d = date.today() + timedelta(days=1)
-    today=date.today()
-    next_thursday = next_weekday(d, 3) # 0 = Monday, 1=Tuesday, 2=Wednesday...
-    tommorw_date=next_thursday.strftime("%d/%m/%y")
-    url = "https://api.mindee.net/v1/products/GreenArmy/screenshot/v1/predict"
-    hours=[]
-
-    if file_path is not None:
-        response = requests.get(file_path)
-        img = Image.open(BytesIO(response.content))
-        image_file = BytesIO()
-        img.save(image_file, format='JPEG')
-        image_file.seek(0)  # important, set pointer to beginning after writing image
-        files = {"document": image_file}
-        headers = {"Authorization": "Token e2f347943462442cc768bd8ab9607149"}
-        response = requests.post(url, files=files, headers=headers)
-        response=response.json()
-        size=len(response["document"]['inference']['pages'][0]['prediction']["wednesday_date"]["values"])
-
-        hours_titles=['sunday_start_time','sunday_end_time','monday_start_time','monday_end_time','tuesday_start_time','tuesday_end_time','wednesday_start_time','wednesday_end_time']
-        start_hour_titles=['sunday_start_time','monday_start_time','tuesday_start_time','wednesday_start_time']
-        end_hour_titles=['sunday_end_time','monday_end_time','tuesday_end_time','wednesday_end_time']
-        start_hour,end_hour=[],[]
-        for hour in hours_titles:
-            values_size=len(response["document"]['inference']['prediction'][hour]["values"])
-            if len(response["document"]['inference']['prediction']['wednesday_date']["values"])>1:
-                    for value in range(values_size):
-                        if response["document"]['inference']['prediction']["wednesday_date"]["values"][value]["content"]=='02':
-                            #if response["document"]['inference']['prediction']["wednesday_date"]["values"][value]["content"]==today_date/yom_reviei:
-                            if response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1.0 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==0.99:
-                                    hours.append(response["document"]['inference']['prediction'][hour]["values"][value]["content"])
-
-            else:
-                for value in range(values_size):
-                    #if response["document"]['inference']['prediction']["wednesday_date"]["values"][value]["content"]==today_date/yom_reviei:
-                    if response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1.0 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==0.99:
-                            hours.append(response["document"]['inference']['prediction'][hour]["values"][value]["content"])
-            
-            total_hours_end, total_minutes_end, hours,minutes=calculate(hours)
-            
-            m.edit_text(text='שבוע טוב, אימא\n''השבוע עבדת '+total_hours_end+' שעות ו־'+total_minutes_end+' דקות.\n''ביום חמישי הקרוב – '+tommorw_date+', תצטרכי לעבוד ' +hours+ ' שעות ו־' +minutes+ ' דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)')
-            
-        else:
-            m.edit_text(text="⚠️Something went wrong, please try again later ⚠️")
-    else:
-        m.edit_text("Something went wrong, Send this image again")
-
-
 def calculate(data_list):
+    print(data_list)
     start_hour,end_hour=[],[]
     for i in range(len(data_list)):
         if i%2:
@@ -121,3 +58,70 @@ def calculate(data_list):
 
     print(total_hours_end, total_minutes_end, total_hours, total_minutes)
     return total_hours_end, total_minutes_end, total_hours, total_minutes
+
+@send_typing_action
+def extract_image(update:Update,context:CallbackContext):
+    '''
+    This function is called when the user sends a photo.
+    '''
+    chat_id=update.effective_chat.id
+    file_id = update.message.photo[-1].file_id
+    print(file_id,'file_id')
+    newFile=context.bot.get_file(file_id)
+    print(newFile,'newFile')
+    file_path= newFile.file_path
+    print(file_path,'file_path')
+    m = update.message.reply_text('מנתח תמונה, רק רגע...',quote=True)
+    
+    print(update.message.photo[-1].height,update.message.photo[-1].width)
+
+    d = date.today() + timedelta(days=1)
+    today=date.today()
+    next_thursday = next_weekday(d, 3) # 0 = Monday, 1=Tuesday, 2=Wednesday...
+    tommorw_date=next_thursday.strftime("%d/%m/%y")
+    url = "https://api.mindee.net/v1/products/GreenArmy/screenshot/v1/predict"
+    hours=[]
+
+    if file_path is not None:
+        response = requests.get(file_path)
+        img = Image.open(BytesIO(response.content))
+        image_file = BytesIO()
+        img.save(image_file, format='JPEG')
+        image_file.seek(0)  # important, set pointer to beginning after writing image
+        files = {"document": image_file}
+        headers = {"Authorization": "Token e2f347943462442cc768bd8ab9607149"}
+        response = requests.post(url, files=files, headers=headers)
+        response=response.json()
+        print(response)
+        size=len(response["document"]['inference']['pages'][0]['prediction']["wednesday_date"]["values"])
+
+        hours_titles=['sunday_start_time','sunday_end_time','monday_start_time','monday_end_time','tuesday_start_time','tuesday_end_time','wednesday_start_time','wednesday_end_time']
+        start_hour_titles=['sunday_start_time','monday_start_time','tuesday_start_time','wednesday_start_time']
+        end_hour_titles=['sunday_end_time','monday_end_time','tuesday_end_time','wednesday_end_time']
+        start_hour,end_hour=[],[]
+
+        for hour in hours_titles:
+            values_size=len(response["document"]['inference']['prediction'][hour]["values"])
+            if len(response["document"]['inference']['prediction']['wednesday_date']["values"])>1:
+                    for value in range(values_size):
+                        if response["document"]['inference']['prediction']["wednesday_date"]["values"][value]["content"]=='02':
+                            #if response["document"]['inference']['prediction']["wednesday_date"]["values"][value]["content"]==today_date/yom_reviei:
+                            if response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1.0 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==0.99:
+                                    hours.append(response["document"]['inference']['prediction'][hour]["values"][value]["content"])
+
+            else:
+                for value in range(values_size):
+                    #if response["document"]['inference']['prediction']["wednesday_date"]["values"][value]["content"]==today_date/yom_reviei:
+                    if response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==1.0 or response["document"]['inference']['prediction'][hour]["values"][value]["confidence"]==0.99:
+                            hours.append(response["document"]['inference']['prediction'][hour]["values"][value]["content"])
+            
+            total_hours_end, total_minutes_end, hours,minutes=calculate(hours)
+            
+            m.edit_text(text='שבוע טוב, אימא\n''השבוע עבדת '+total_hours_end+' שעות ו־'+total_minutes_end+' דקות.\n''ביום חמישי הקרוב – '+tommorw_date+', תצטרכי לעבוד ' +hours+ ' שעות ו־' +minutes+ ' דקות כדי להגיע למכסת 29 השעות השבועיות.\nשיהיה לך המשך שבוע נפלא :)')
+            
+        else:
+            m.edit_text(text="⚠️Something went wrong, please try again later ⚠️")
+    else:
+        m.edit_text("Something went wrong, Send this image again")
+
+
